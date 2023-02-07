3dc86ddd449f5264d6de67d9e4cf750fe000156f
no3utufff89 <14198908@mail.rugit config --global core.autocrlf truegit config --global core.safecrlf warngit config --global core.quotepath offgit add .git statusgit config --global user.email 14198908@mail.ru>
2023-02-07 23:46:23 +0300

    Исправил слайдер

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,38 @@
-$(document).ready(function(){
+$(function() {
+
     $('.advantages-list').slick({
-        mobileFirst: true,
-        slidesToShow: 1,
-        responsive: [
-            {
-                breakpoint: 768,
-                slidesToShow: 2
-            },
-            {
-                breakpoint: 1000,
-                settings: "unslick"
-            },
+    
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
             
-                    ]
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+       
+      ]
     });
-});
+    });
+
+
